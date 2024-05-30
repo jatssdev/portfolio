3a278f0567f274d4bb6641de6d91823887fe78a3
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-05-30 15:58:06 +0530

    Portfolio using next js , react and tailwind

---
+++ src/constants/index.ts
@@ -0,0 +1,246 @@
+import type {
+  TNavLink,
+  TService,
+  TTechnology,
+  TExperience,
+  TTestimonial,
+  TProject,
+} from "../types";
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+
+  starbucks,
+
+  maxgen,
+
+  carrent,
+  jobit,
+  tripguide,
+
+} from "../assets";
+
+export const navLinks: TNavLink[] = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services: TService[] = [
+  {
+    title: "Mern Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Php Developer",
+    icon: mobile,
+  },
+  {
+    title: "Next.js Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies: TTechnology[] = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+
+];
+
+const experiences: TExperience[] = [
+  {
+    title: "Mern Stack Developer",
+    companyName: "Maxgen Technologies Pvt.Ltd",
+    icon: maxgen,
+    iconBg: "#383E56",
+    date: "July 2022 - Present",
+    points: [
+      "Developing and maintaining web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to deliver high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility for all applications.",
+      "Participating in code reviews and providing constructive feedback to enhance code quality and team performance."
+
+
+    ],
+  },
+  {
+    title: "React, Mern , PHP Trainee",
+    companyName: "Coding Cloud Institute",
+    icon: maxgen,
+    iconBg: "#E6DEDD",
+    date: "July 2022 - Present",
+    points: [
+      "Worked as a trainee, gaining hands-on experience in PHP, MERN stack, and React.",
+      "Trained and mentored numerous developers and interns in PHP, MERN stack, and React technologies.",
+      "Developed and delivered training programs to enhance the skills of new developers and interns.",
+      "Provided guidance and support to ensure the successful completion of projects by trainees and interns.",
+      "Fostered a collaborative learning environment, helping team members improve their technical expertise and problem-solving abilities."
+    ],
+  },
+
+];
+
+const testimonials: TTestimonial[] = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects: TProject[] = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    sourceCodeLink: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
